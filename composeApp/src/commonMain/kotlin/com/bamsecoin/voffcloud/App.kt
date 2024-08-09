7053f95846606769c0e17c06bd71d49074382c10
Jørgen Svennevik Notland <jorgen.notland@protonmail.com>
2024-08-09 21:13:16 +0200

    Bamsecoin tx data in list in UX

--- composeApp/src/commonMain/kotlin/com/bamsecoin/voffcloud/App.kt
+++ composeApp/src/commonMain/kotlin/com/bamsecoin/voffcloud/App.kt
@@ -1,10 +1,10 @@
 package com.bamsecoin.voffcloud
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -16,19 +16,14 @@ import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.request.header
-import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.bodyAsChannel
-import io.ktor.client.statement.bodyAsText
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import voffcloud.composeapp.generated.resources.Res
-import voffcloud.composeapp.generated.resources.compose_multiplatform
 
 @Serializable
 data class Product(
@@ -51,7 +46,8 @@ data class Transaction(
     val reversalType: String? = null  // This field is optional
 )
 
-suspend fun getTransactions() {
+val transactions = MutableStateFlow<List<Transaction>>(listOf())
+fun getTransactions() = GlobalScope.launch(Dispatchers.Default){
     val client = HttpClient(CIO) {
         install(ContentNegotiation) {
             json(Json {
@@ -61,66 +57,51 @@ suspend fun getTransactions() {
             })
         }
     }
-
-    // Retrieve the environment variables
-    val serverPath = "https://fridge-api.babel.as/api/pos/allTransactions"  // Fallback URL if env is missing
-
-    // Make the request with the environment variables
-    val response: HttpResponse = client.get("https://fridge-api.babel.as/api/pos/allTransactions") {
-        // TODO: Move to env before commit
-        header("Authorization", "Bearer ${Keys().bamseCoinApi}")
-    }
-    val body = response.bodyAsText()
-
     try {
         // Deserialize the response into a list of Transaction objects
-        val transactions: List<Transaction> = client.get("https://fridge-api.babel.as/api/pos/allTransactions") {
+        val result: List<Transaction> = client.get("https://fridge-api.babel.as/api/pos/allTransactions") {
             header("Authorization", "Bearer ${Keys().bamseCoinApi}")
         }.body()
 
         // Process each transaction as needed
-        transactions.forEach { transaction ->
+        result.forEach { transaction ->
             println(transaction)
         }
+
+        transactions.value = result.sortedBy { it.timestamp }.reversed()
     } catch (e: Exception) {
         println("Error occurred: ${e.message}")
     } finally {
         client.close()
     }
-
-
-    // Process the response as needed
-    println(body)
-    println(response.status)
-
-    /*
-
-    val body = response.body<List<Transaction>>()
-    body.forEach { transaction ->
-        println(transaction)
-    }
-     */
 }
 
 @Composable
 @Preview
 fun App() {
+    getTransactions()
 
-    GlobalScope.launch(Dispatchers.Main) {
-        getTransactions()
-    }
+    val transactions = transactions.collectAsState()
 
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        Column(
+            Modifier.fillMaxWidth().verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            transactions.value.forEach {
+                Card {
+                    Column {
+                        Text(it.timestamp.toString())
+                        Text(it.subtotal.toString())
+                        it.products.forEach { product ->
+                            Text(product.productLabel)
+                            Text(product.count.toString())
+                        }
+                        Text(it.type)
+                        Text(it.receiptUrl ?: "")
+                        Text(it.reversalId ?: "")
+                        Text(it.reversalType ?: "")
+                    }
                 }
             }
         }
